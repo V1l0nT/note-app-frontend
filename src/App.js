65133e70e6be1c0722859551957c3b32f6467fd0
Nikita Vilkov <nik.vilkov.2004@mail.ru>
2025-05-24 17:47:36 +0300

    Fix routing for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Navigation from './components/Navigation';
 import Login from './components/Login';
